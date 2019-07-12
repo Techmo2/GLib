9f78e55e33bed4fb4b37036a9ad48a2450687135
Bradly Tillmann <bradlytillmann@gmail.com>
2019-07-12 12:03:24 -0600

    Fixed dependency issues.

--- sources/garrysmod_common-master/source/ABICompatibility.cpp
+++
@@ -1,7 +0,0 @@
-#include <Platform.hpp>
-
-#if defined SYSTEM_WINDOWS && ( _MSC_VER < 1910 || _MSC_VER > 1999 )
-
-#error The only supported compilation platform for this project on Windows is Visual Studio 2017 (for ABI compatibility reasons).
-
-#endif
